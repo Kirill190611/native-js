69cb113d9dcf9b49a6c6704f9a4e767cd6a1a9f1
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-04-06 20:44:18 +0300

    Additional practice with deep copy of object

--- src/10-immutability/10.test.ts
+++ src/10-immutability/10.test.ts
@@ -1,15 +1,17 @@
 import {
+    addCompany,
+    addedNewBook, addedNewBooksToUser, CompanyType,
     makeHairstyle,
-    moveUser,
+    moveUser, moveUserToOtherHouse, removeUserBook, updateCompany, updateCompanyTitle, updateUserBook,
     upgradeUserLaptop,
     upgradeUserLaptopRam,
     upgradeUserLaptopVideocardVersion,
-    UserType,
+    UserType, UserWithBooksType,
     UserWIthLaptopType
 } from "./10";
 
 test("reference type test", () => {
-    let user: UserType = {
+    let user: UserWIthLaptopType & UserWithBooksType = {
         name: "Kirill",
         hair: 30,
         address: {
@@ -26,7 +28,17 @@ test("reference type test", () => {
                 },
             },
         },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
     }
+
 
     const awesomeUser = makeHairstyle(user, 2)
 
@@ -35,7 +47,7 @@ test("reference type test", () => {
 })
 
 test("change address", () => {
-    let user: UserWIthLaptopType = {
+    let user: UserWIthLaptopType & UserWithBooksType = {
         name: "Kirill",
         hair: 30,
         address: {
@@ -52,7 +64,17 @@ test("change address", () => {
                 },
             },
         },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
     }
+
 
     const movedUser = moveUser(user, 'Moscow')
 
@@ -65,7 +87,7 @@ test("change address", () => {
 })
 
 test("upgrade laptop to macbook", () => {
-    let user: UserWIthLaptopType = {
+    let user: UserWIthLaptopType & UserWithBooksType = {
         name: "Kirill",
         hair: 30,
         address: {
@@ -82,7 +104,17 @@ test("upgrade laptop to macbook", () => {
                 },
             },
         },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
     }
+
 
     const upgradedUser = upgradeUserLaptop(user, 'Macbook')
 
@@ -95,7 +127,7 @@ test("upgrade laptop to macbook", () => {
 })
 
 test("upgrade laptop RAM", () => {
-    let user: UserWIthLaptopType = {
+    let user: UserWIthLaptopType & UserWithBooksType = {
         name: "Kirill",
         hair: 30,
         address: {
@@ -112,7 +144,17 @@ test("upgrade laptop RAM", () => {
                 },
             },
         },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
     }
+
 
     const upgradedUser = upgradeUserLaptopRam(user, 32)
 
@@ -123,7 +165,7 @@ test("upgrade laptop RAM", () => {
 })
 
 test("upgrade laptop video driver version", () => {
-    let user: UserWIthLaptopType = {
+    let user: UserWIthLaptopType & UserWithBooksType = {
         name: "Kirill",
         hair: 30,
         address: {
@@ -140,7 +182,17 @@ test("upgrade laptop video driver version", () => {
                 },
             },
         },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
     }
+
 
     const upgradedUser = upgradeUserLaptopVideocardVersion(user, "17.2")
 
@@ -148,4 +200,329 @@ test("upgrade laptop video driver version", () => {
     expect(user.laptop.parts.videocard.version).not.toBe(upgradedUser.laptop.parts.videocard.version);
     expect(user.laptop.parts.videocard.version).toBe("15.5");
     expect(upgradedUser.laptop.parts.videocard.version).toBe("17.2");
+})
+
+test("upgrade house number", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
+    }
+
+
+    const upgradedUser = moveUserToOtherHouse(user, 81)
+
+    expect(user).not.toBe(upgradedUser);
+    expect(upgradedUser.address.house).toBe(81);
+    expect(user.books).toBe(upgradedUser.books);
+})
+
+test("add new book", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
+    }
+
+
+    const upgradedUser = addedNewBook(user, "ts")
+
+    expect(upgradedUser.books.length).toBe(5);
+    expect(upgradedUser.books[4]).toBe("ts");
+})
+
+test("add new books", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
+    }
+
+
+    const upgradedUser = addedNewBooksToUser(user, ["ts", "redux",])
+
+    expect(upgradedUser.books.length).toBe(6);
+
+})
+
+test("update js book to ts", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
+    }
+
+
+    const upgradedUser = updateUserBook(user, "js", "ts")
+
+    expect(upgradedUser.books.length).toBe(4);
+    expect(upgradedUser.books[2]).toBe("ts");
+})
+
+test("remove book", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+        ],
+    }
+
+
+    const upgradedUser = removeUserBook(user, "js")
+
+    expect(upgradedUser.books.length).toBe(3);
+    expect(upgradedUser.books[2]).toBe("react");
+})
+
+test("add company", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Apple",
+                id: 1,
+            },
+            {
+                title: "Google",
+                id: 2,
+            },
+        ],
+    }
+
+    const upgradedUser = addCompany(user, {title: "Yandex", id: 3})
+
+    expect(upgradedUser.companies.length).toBe(3);
+    expect(upgradedUser.companies[2].title).toBe("Yandex");
+    expect(upgradedUser.companies[2].id).toBe(3);
+})
+
+test("update company", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Appleswq",
+                id: 1,
+            },
+            {
+                title: "Google",
+                id: 2,
+            },
+        ],
+    }
+
+    const upgradedUser = updateCompanyTitle(user, 1, "Apple")
+
+    expect(upgradedUser.companies.length).toBe(2);
+    expect(upgradedUser.companies[0].title).toBe("Apple");
+    expect(upgradedUser.companies[0].id).toBe(1);
+})
+
+test("update company 123", () => {
+    let user: UserWIthLaptopType & UserWithBooksType = {
+        name: "Kirill",
+        hair: 30,
+        address: {
+            title: "Obninsk",
+            house: 12,
+        },
+        laptop: {
+            title: "HP",
+            parts: {
+                ram: 16,
+                videocard: {
+                    title: "Nvidia",
+                    version: "15.5",
+                },
+            },
+        },
+        books: [
+            "css", "html", "js", "react",
+        ],
+        companies: [
+            {
+                title: "Appleswq",
+                id: 1,
+            },
+            {
+                title: "Google",
+                id: 2,
+            },
+        ],
+    }
+
+    let companies: CompanyType = {
+        'Kirill': [
+            {
+                title: "ABC",
+                id: 1,
+            },
+            {
+                title: "CBA",
+                id: 2,
+            },
+        ],
+        'Viktor': [
+            {
+                title: "QWE",
+                id: 1,
+            },
+            {
+                title: "RTY",
+                id: 2,
+            },
+        ],
+    }
+
+    const upgradedUser = updateCompany(companies, 'Kirill', 2, "Yandex")
+
+    expect(upgradedUser['Kirill']).not.toBe(companies['Kirill']);
+    expect(upgradedUser['Viktor']).toBe(companies['Viktor']);
+    expect(upgradedUser['Kirill'][1].title).toBe('Yandex');
+    expect(upgradedUser['Kirill'][1].id).toBe(2);
 })
--- src/10-immutability/10.ts
+++ src/10-immutability/10.ts
@@ -1,8 +1,19 @@
 export type UserType = {
     name: string
     hair: number
-    address: {title: string, house: number}
+    address: { title: string, house: number }
     laptop: LaptopType
+    books: Array<string>
+    companies: Array<CompaniesType>
+}
+
+export type CompanyType = {
+    [key: string]: Array<CompaniesType>
+}
+
+export type CompaniesType = {
+    title: string
+    id: number
 }
 
 export type VideoCardType = {
@@ -22,6 +33,10 @@ export type LaptopType = {
 
 export type UserWIthLaptopType = UserType & {
     laptop: LaptopType
+}
+
+export type UserWithBooksType = UserType & {
+    books: Array<string>
 }
 
 export const makeHairstyle = (u: UserType, qty: number) => {
@@ -68,4 +83,76 @@ export const upgradeUserLaptopVideocardVersion = (u: UserType, version: string) 
             }
         }
     }
+}
+
+export const moveUserToOtherHouse = (u: UserType, house: number) => {
+    return {
+        ...u,
+        address: {
+            ...u.address, house
+        }
+    }
+}
+
+export const addedNewBook = (u: UserType, book: string) => {
+    return {
+        ...u,
+        books: [
+            ...u.books, book
+        ]
+    }
+}
+
+export const addedNewBooksToUser = (u: UserType, books: Array<string>) => {
+    return {
+        ...u,
+        books: [
+            ...u.books.concat(books)
+        ]
+    }
+}
+
+export const updateUserBook = (u: UserType, oldBook: string, updateBook: string) => {
+    return {
+        ...u,
+        books: u.books.map(book => book === oldBook ? updateBook : book)
+    }
+}
+
+export const removeUserBook = (u: UserType, removeBook: string) => {
+    return {
+        ...u,
+        books: u.books.filter(book => book !== removeBook)
+    }
+}
+
+export const addCompany = (u: UserType, company: CompaniesType) => {
+    const newCompany = {
+        title: company.title,
+        id: company.id,
+    }
+
+    return {
+        ...u,
+        companies: [...u.companies, newCompany]
+    }
+}
+
+export const updateCompanyTitle = (u: UserType, companyId: number, updatedTitle: string) => {
+    return {
+        ...u,
+        companies: u.companies.map(company => company.id === companyId ? {...company, title: updatedTitle} : company)
+    }
+}
+
+export const updateCompany = (companies: CompaniesType,
+                              userName: string,
+                              companyId: number,
+                              newTitle: string) => {
+    // @ts-ignore
+    return {
+        ...companies,
+        [userName]: companies[userName].map(c => c.id === companyId ? {...c, title: newTitle} : c)
+    }
+
 }
